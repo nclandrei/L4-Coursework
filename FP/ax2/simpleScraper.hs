f100b7857f358416e138af9b5854359790b0aa12
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-11-20 01:05:13 +0000

    Built skeleton for the project.
    Added initial necessary classes, as well as implemented a bit of the client.

--- FP/ax2/simpleScraper.hs
+++ FP/ax2/simpleScraper.hs
@@ -3,14 +3,20 @@
 import Text.HTML.Scalpel
 
 type Name = String
+type TelNumber = String
 data Person 
-    = PersonInfo Name URL
+    = PersonInfo Name TelNumber
     deriving (Show, Eq)
+
+getUrl :: [[Person]] -> String
+getUrl (PersonInfo _ number) = number
 
 main :: IO ()
 main = do
     personsList <- scrapeURL "http://www.gla.ac.uk/schools/computing/staff/" scrapeList
+    numbers <- scrapeURL  scrapePerson
     print personsList
+    print numbers
 
 scrapeList :: Scraper String [[Person]]
 scrapeList = 
@@ -23,5 +29,14 @@ scrapeUl =
 scrapeLi :: Scraper String Person
 scrapeLi = do
     personName <- text $ "a"
-    personURL  <- attr "href" $ "a"
-    return $ PersonInfo personName personURL
+    personNumber <- attr "href" $ "a"
+    return $ PersonInfo personName personNumber
+
+scrapePerson :: Scraper String [TelNumber]
+scrapePerson = 
+    chroots ("div" @: ["id" @= "sp_contactInfo"]) scrapeNumber
+
+scrapeNumber :: Scraper String TelNumber
+scrapeNumber = do
+    num <- text $ "p"
+    return num

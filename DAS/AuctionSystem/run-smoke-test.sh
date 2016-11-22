bd03cb24b449a5556e37d6cc8b514304ed1bf1bc
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-11-22 23:35:11 +0000

    Removed old file for smoke testing

--- DAS/AuctionSystem/run-smoke-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -classpath bin/test-smoke.jar:bin/server.jar:bin/api.jar:bin/common.jar:bin/client.jar:libs/junit-4.12.jar:libs/hamcrest-core-1.3.jar -Djava.security.policy=client.policy -Djava.rmi.server.codebase=file:bin/api.jar org.junit.runner.JUnitCore smoke.SmokeTest

b3c8495e6284363dacca86d3631a3b49611d6267
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-11-25 05:11:04 +0000

    Managed to almost write function that would retrieve all URLs inside the tab

--- DAS/AuctionSystem/run-client.sh
+++ DAS/AuctionSystem/run-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp bin/client.jar:bin/api.jar:bin/common.jar -Djava.security.policy=client.policy -Djava.rmi.server.codebase=file:bin/api.jar uk.ac.gla.das.rmi.auctionsystem.client.AuctionClient localhost 1099
+java -cp bin/client.jar:bin/api.jar:bin/common.jar -Djava.security.policy=client.policy -Djava.rmi.server.codebase=file:bin/api.jar uk.ac.gla.das.rmi.auctionsystem.client.AuctionClient 178.62.126.190 1099
--- FP/ax2/simpleScraper.hs
+++ FP/ax2/simpleScraper.hs
@@ -2,41 +2,39 @@
 
 import Text.HTML.Scalpel
 
-type Name = String
-type TelNumber = String
-data Person 
-    = PersonInfo Name TelNumber
-    deriving (Show, Eq)
-
-getUrl :: [[Person]] -> String
-getUrl (PersonInfo _ number) = number
-
 main :: IO ()
 main = do
-    personsList <- scrapeURL "http://www.gla.ac.uk/schools/computing/staff/" scrapeList
-    numbers <- scrapeURL  scrapePerson
+    personsList <- scrapeURL "http://www.gla.ac.uk/schools/computing/staff/" scrapeList 
+    case personsList of
+        Just list -> do
+                        print list
+                        print (getURLs list)
+        Nothing -> print "Text could not be retrieved!"
     print personsList
-    print numbers
+    
 
-scrapeList :: Scraper String [[Person]]
+scrapeList :: Scraper String [[(String, String)]]
 scrapeList = 
     chroots ("div" @: ["id" @= "research-teaching"]) scrapeUl
 
-scrapeUl :: Scraper String [Person]
+scrapeUl :: Scraper String [(String, String)]
 scrapeUl =
     chroots "li" scrapeLi
 
-scrapeLi :: Scraper String Person
+scrapeLi :: Scraper String (String, String)
 scrapeLi = do
     personName <- text $ "a"
-    personNumber <- attr "href" $ "a"
-    return $ PersonInfo personName personNumber
+    url <- attr "href" $ "a"
+    return (personName, url)
 
-scrapePerson :: Scraper String [TelNumber]
+scrapePerson :: Scraper String [String]
 scrapePerson = 
     chroots ("div" @: ["id" @= "sp_contactInfo"]) scrapeNumber
 
-scrapeNumber :: Scraper String TelNumber
+scrapeNumber :: Scraper String String
 scrapeNumber = do
     num <- text $ "p"
     return num
+
+getURLs :: [[(String, String)]] -> [[String]]
+getURLs [[(_, url)]] = [[url]]

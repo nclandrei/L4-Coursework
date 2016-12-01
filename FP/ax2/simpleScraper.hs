2bd73e307f3f936b4983cae407a84a4c4713443a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-12-01 13:22:59 +0000

    Added HaTeX package to cabal and program is now able to write simple tex file into directory

--- FP/ax2/simpleScraper.hs
+++ FP/ax2/simpleScraper.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Text.HTML.Scalpel
-
+import Text.LaTeX
 
 main :: IO ()
 main = do
     personsList <- scrapeURL "http://www.gla.ac.uk/schools/computing/staff/" scrapeList 
     case personsList of
         Just list -> do
-                        print list
-                        let l = list !! 0
-                        personsNumbers <- scrapeURL (getURLs (l !! 1)) scrapePerson
-                        print personsNumbers
+            print list
+            let urls = map (\(x,y) -> y) (head list)
+            let personNumbers = map scrapeURLs urls
+            execLaTeXT simple >>= renderFile "directory.tex"
         Nothing -> print "Text could not be retrieved!"
 
 scrapeList :: Scraper String [[(String, String)]]
@@ -26,7 +26,9 @@ scrapeLi :: Scraper String (String, String)
 scrapeLi = do
     personName <- text $ "a"
     url <- attr "href" $ "a"
-    return (personName, url)
+    let fullurl = "http://www.gla.ac.uk" ++ url
+    let personNumber = scrapeURL fullurl scrapePerson
+    return (personName, fullurl)
 
 scrapePerson :: Scraper String [String]
 scrapePerson = 
@@ -37,5 +39,30 @@ scrapeNumber = do
     num <- text $ "p"
     return num
 
-getURLs :: (String, String) -> String
-getURLs (_, url) = ("http://www.gla.ac.uk" ++ url)
+scrapeURLs :: String -> IO (Maybe [String])
+scrapeURLs x = scrapeURL x scrapePerson
+
+simple :: Monad m => LaTeXT_ m
+simple = do
+ thePreamble
+ document theBody
+
+ -- Preamble with some basic info.
+thePreamble :: Monad m => LaTeXT_ m
+thePreamble = do
+ documentclass [] article
+ author "Andrei-Mihai Nicolae"
+ title "Functional Programming -- Assessed Exercise 2"
+
+-- Body with a section.
+theBody :: Monad m => LaTeXT_ m
+theBody = do
+ maketitle
+ section "Hello"
+ "This is a simple example using the "
+ hatex
+ " library. "
+ textbf "Enjoy!"
+ " "
+ -- This is how we nest commands.
+ textbf (large "Yoohoo!")

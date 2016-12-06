9814add05d27a1afa74b9cfe56b6866695649ea4
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2016-12-06 16:21:45 +0200

    Started adding comments to the scraper

--- FP/ax2/scraper.hs
+++ FP/ax2/scraper.hs
@@ -14,20 +14,28 @@ import Control.Monad
 main :: IO ()
 main = do
     let universityURL = "http://www.gla.ac.uk/schools/computing/staff/"
+    print "---> Starting to retrieve research and teaching persons list..."
     researchPersonsList <- scrapeURL universityURL scrapeResearchList
+    print "---> Research and teaching persons list retrieved! Starting to retrieve management and support persons list..."
     managementPersonsList <- scrapeURL universityURL scrapeManagementList
+    print "---> Management and support persons list retrieved! Starting to retrieve affiliate persons list..."
     affiliatePersonsList <- scrapeURL universityURL scrapeAffiliateList
+    print "---> Affiliate persons list retrieved! Starting to retrieve honorary persons list..."
     honoraryPersonsList <- scrapeURL universityURL scrapeHonoraryList
+    print "---> Honorary persons list retrieved! Starting to sort and remove duplicates from the full list of people..."
     let validResearchPersonsList = fromMaybe [] researchPersonsList
     let validManagementPersonsList = fromMaybe [] managementPersonsList
     let validAffiliatePersonsList = fromMaybe [] affiliatePersonsList
     let validHonoraryPersonsList = fromMaybe [] honoraryPersonsList
     let fullValidList = (validResearchPersonsList !! 0) ++ (validManagementPersonsList !! 0) ++ (validAffiliatePersonsList !! 0) ++ (validHonoraryPersonsList !! 0)
     let sortedAndNoDuplicatesList = removeDuplicates fullValidList
+    print  "---> Sorting and removing duplicates completed! Starting to append corresponding phone numbers to all people..."
     contacts <- mapM scrapeContactURL sortedAndNoDuplicatesList
+    print "---> All phone numbers retrieved! Starting to perform filtering and removing people with incorrect phone numbers..."
     let peopleWithoutEmptyInfo = filter removePeopleWithoutNumbers contacts
     let contactsWithHeadOfList = map removePhoneNumberList peopleWithoutEmptyInfo
     let contactsWithTelephone = filter (\(x,y) -> (isInfixOf "telephone" y)) contactsWithHeadOfList
+    print  "---> Filtering and removing redundant information completed! Starting to retrieve only the phone number without any other text..."
     let finalContacts = map getPhoneNumberFromString contactsWithTelephone
     let records = finalContacts
     body <- execLaTeXT (constructDocument records)
@@ -64,9 +72,9 @@ scrapePerson :: Scraper String [String]
 scrapePerson = 
     chroots ("div" @: ["id" @= "sp_contactInfo"]) scrapeNumber
 
-scrapePersonTwo :: Scraper String String
+scrapePersonTwo :: Scraper String [String]
 scrapePersonTwo = do
-    content <- text $ "p" @: ["style" @= "margin: 0 0 10px 25px; padding: 5px; color: ##333;"]
+    content <- texts $ "p" @: ["style" @= "margin: 0 0 10px 25px; padding: 5px; color: ##333;"]
     return content
 
 scrapeNumber :: Scraper String String
@@ -74,18 +82,15 @@ scrapeNumber = do
     num <- text $ "p"
     return num
 
+scrapeContactURL :: (String, URL) -> IO (String, [String])
 scrapeContactURL (x,y) = do
     phoneNumber <- scrapeURL y scrapePerson
-    case phoneNumber of
-        Just number -> do
-            return (x, number)
-        Nothing -> do
-            print "am ajuns aici"
-            alternativeNumber <- scrapeURL y scrapePersonTwo
-            let validPhoneNumber = fromMaybe "" alternativeNumber
-            return (x, [validPhoneNumber])
---    let validPhoneNumber = fromMaybe [""] phoneNumber
---    return (x, validPhoneNumber)
+    let validPhoneNumber = fromMaybe [""] phoneNumber
+    if (validPhoneNumber /= []) then return (x, validPhoneNumber)
+        else do
+            secondPhoneNumber <- scrapeURL y scrapePersonTwo
+            let validSecondPhoneNumber = fromMaybe [""] secondPhoneNumber
+            return (x, validSecondPhoneNumber)
 
 removeDuplicates :: (Ord a) => [a] -> [a]
 removeDuplicates = map head . group . sort

de1978ede506e8e9c488e2c93458abde356266ff
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-02-19 21:18:15 +0000

    changed the skeleton for the driver

--- BD/ax1/src/main/java/uk/ac/glasgow/bd/MyMRDriver.java
+++ BD/ax1/src/main/java/uk/ac/glasgow/bd/MyMRDriver.java
@@ -9,12 +9,27 @@ import org.apache.hadoop.util.Tool;
 
 public class MyMRDriver extends Configured implements Tool {
 
-    @Override
-    public int run(String[] strings) throws Exception {
-        Configuration conf = new Configuration(getConf());
-        conf.addResource(new Path("/local/bd4/bd4-hadoop-ug/conf/core-site.xml"));
-        Job job = Job.getInstance(conf);
-        return 0;
+    public class WordCount extends Configured implements Tool {
+        public int run(String[] args) throws Exception {
+            Job job = new Job();
+            job.setJobName("MyWordCount(" + args[0] + ")");
+            job.setJarByClass(WordCount.class);
+            job.setInputFormatClass(MyInputFormat.class); job.setOutputFormatClass(TextOutputFormat.class);
+            job.setMapperClass(MyMapper.class);
+            job.setPartitionerClass(MyPartitioner.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(IntWritable.class);
+            job.setReducerClass(MyReducer.class);
+            job.setCombinerClass(MyReducer.class);
+            FileInputFormat.setInputPaths(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(job.getJobName() +
+                    "_output"));
+            job.submit();
+            return job.waitForCompletion(true) ? 0 : 1;
+        }
+        public static void main(String[] args) throws Exception {
+            System.exit(ToolRunner.run(new WordCount(), args));
+        }
     }
 
 }

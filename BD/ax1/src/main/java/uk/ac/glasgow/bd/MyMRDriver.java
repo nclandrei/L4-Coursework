14633d07a9d908fa1678a13fd9a3e611b854e489
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-02-19 20:50:37 +0000

    added initial config for the map reduce driver

--- BD/ax1/src/main/java/uk/ac/glasgow/bd/MyMRDriver.java
+++ BD/ax1/src/main/java/uk/ac/glasgow/bd/MyMRDriver.java
@@ -1,7 +1,20 @@
 package main.java.uk.ac.glasgow.bd;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
 
-public class MyMRDriver {
+
+public class MyMRDriver extends Configured implements Tool {
+
+    @Override
+    public int run(String[] strings) throws Exception {
+        Configuration conf = new Configuration(getConf());
+        conf.addResource(new Path("/local/bd4/bd4-hadoop-ug/conf/core-site.xml"));
+        Job job = Job.getInstance(conf);
+        return 0;
+    }
 
 }

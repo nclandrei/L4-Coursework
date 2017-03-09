6472ee48306320118a65da584073d01c5780405f
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-09 14:01:47 +0000

    final version for the min dist test

--- IR/ax2/src/test/java/uk/ac/gla/dcs/dsms/TestMinDistProximityFeatureDSM.java
+++ IR/ax2/src/test/java/uk/ac/gla/dcs/dsms/TestMinDistProximityFeatureDSM.java
@@ -12,30 +12,39 @@ import org.terrier.structures.postings.IterablePosting;
 import org.terrier.tests.ApplicationSetupBasedTest;
 import org.terrier.utility.ApplicationSetup;
 
+/**
+ * Test class for the min_dist proximity feature DSM
+ * @author 2147392n
+ */
 public class TestMinDistProximityFeatureDSM extends ApplicationSetupBasedTest {
 	@Test
     public void testOneDocTwoTerms() throws Exception {
         ApplicationSetup.setProperty("termpipelines", "");
 		Index index = IndexTestUtils.makeIndexBlocks(
 				new String[]{"doc1"},
-				new String[]{"The quick brown fox jumps over the quick dog"});
+				new String[]{"The lazy brown fox jumps over the lazy dog"});
 
-		IterablePosting[] ips = new IterablePosting[2];
+		IterablePosting[] ips = new IterablePosting[3];
 
-		System.out.println(index.getLexicon().getLexiconEntry("quick"));
-		System.out.println(index.getLexicon().getLexiconEntry("fox"));
+        System.out.println(index.getLexicon().getLexiconEntry("fox"));
+        System.out.println(index.getLexicon().getLexiconEntry("jumps"));
+		System.out.println(index.getLexicon().getLexiconEntry("lazy"));
 
-		ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("quick"));
-		ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[0] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("fox"));
+        ips[1] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("jumps"));
+		ips[2] = index.getInvertedIndex().getPostings(index.getLexicon().getLexiconEntry("lazy"));
 
 		ips[0].next();
 		ips[1].next();
+        ips[2].next();
 
 		assertEquals(0, ips[0].getId());
 		assertEquals(0, ips[1].getId());
+        assertEquals(0, ips[2].getId());
 
-		System.out.println("Positions of term 'quick'="+ Arrays.toString( ((BlockPosting)ips[0]).getPositions()));
-		System.out.println("Positions of term 'fox'="+ Arrays.toString( ((BlockPosting)ips[1]).getPositions()));
+        System.out.println("Positions of term 'fox'="+ Arrays.toString( ((BlockPosting)ips[0]).getPositions()));
+        System.out.println("Positions of term 'jumps'="+ Arrays.toString( ((BlockPosting)ips[1]).getPositions()));
+		System.out.println("Positions of term 'lazy'="+ Arrays.toString( ((BlockPosting)ips[2]).getPositions()));
 
 		MinDistProximityFeatureDSM minDistProximityFeatureDSM = new MinDistProximityFeatureDSM();
 
@@ -47,7 +56,7 @@ public class TestMinDistProximityFeatureDSM extends ApplicationSetupBasedTest {
 		assertEquals(0.0d, score, 0.0d);
 
 		score = minDistProximityFeatureDSM.calculateDependence(ips, new boolean[]{true,true,true},
-                new double[]{1d, 1d}, false);
+                new double[]{1d, 1d, 1d}, false);
 		assertEquals(2.0d, score, 0.0d);
 	}
 }

2493eca222658927ffce5a1b52575b65ce1e325a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-09 11:14:42 +0000

    improvements on min dist proximity feature class

--- IR/ax2/src/main/java/uk/ac/gla/dcs/dsms/MinDistProximityFeatureDSM.java
+++ IR/ax2/src/main/java/uk/ac/gla/dcs/dsms/MinDistProximityFeatureDSM.java
@@ -2,79 +2,63 @@ package uk.ac.gla.dcs.dsms;
 
 import org.terrier.structures.postings.BlockPosting;
 import org.terrier.structures.postings.IterablePosting;
-
 import org.terrier.matching.dsms.DependenceScoreModifier;
 
+/**
+ *
+ */
 public class MinDistProximityFeatureDSM extends DependenceScoreModifier {
 
-	@Override
-	protected double calculateDependence(
-			IterablePosting[] ips,
-			boolean[] okToUse,
-			double[] phraseTermWeights, boolean SD
-		) 
-	{
-		
-		int numberOfPairs = 0;
+    @Override
+    protected double calculateDependence(IterablePosting[] ips, boolean[] okToUse,
+                                         double[] phraseTermWeights, boolean SD) {
+
+        int numberOfPairs = 0;
 
 
-		double score = 0.0d;  
-		for (int i=0; i< ips.length -1 ; i++)
-		{
-			if (okToUse[i])
-			{
-				for (int j = i+1; j< ips.length ; j++)
-				{
-					if (okToUse[j])
-					{
-						numberOfPairs +=1;
-						BlockPosting firstBP = (BlockPosting) ips[i]; 
-						int[]firstPosList = firstBP.getPositions();
-						BlockPosting secondBP = (BlockPosting) ips[j]; 
-						int[]secondPosList = secondBP.getPositions();
-						
-						score += getMinDistBetweenPostingLists(firstPosList, secondPosList);
-					}
-				}
-			}
+        double score = 0.0d;
+        for (int i=0; i< ips.length -1 ; i++) {
+            if (okToUse[i]) {
+                for (int j = i+1; j< ips.length ; j++) {
+                    if (okToUse[j]) {
+                        numberOfPairs +=1;
+                        BlockPosting firstBP = (BlockPosting) ips[i];
+                        int[]firstPosList = firstBP.getPositions();
+                        BlockPosting secondBP = (BlockPosting) ips[j];
+                        int[]secondPosList = secondBP.getPositions();
+                        score += getMinDistBetweenPostingLists(firstPosList, secondPosList);
+                    }
+                }
+            }
+        }
 
-		}
-		
-		if (numberOfPairs == 0)
-		{
-			return 0.0d;
-		}
-		score /= numberOfPairs;	
-		return score;
-	}
+        return (numberOfPairs == 0) ? 0.0d : score/numberOfPairs;
+    }
 
-	private double getMinDistBetweenPostingLists(int[]firstPosList, int[]secondPosList) {
-		double min = Double.MAX_VALUE;
-		
-		for (int k=0; k< firstPosList.length;  k++)
-		{
-			
-			for (int l=0; l< secondPosList.length; l++)
-			{
-				double dist = Math.abs(secondPosList[l]-firstPosList[k]);
-				if (min > dist)
-				{
-					min = dist;
-				}
-			}
-		}
-		return min;
-	}
+    private double getMinDistBetweenPostingLists(int[]firstPosList, int[]secondPosList) {
+        double min = Double.MAX_VALUE;
 
-	@Override
-	protected double scoreFDSD(int matchingNGrams, int docLength) {
-		throw new UnsupportedOperationException();
-	}
+        for (int k=0; k< firstPosList.length;  k++) {
+            for (int l=0; l< secondPosList.length; l++) {
+                double dist = Math.abs(secondPosList[l]-firstPosList[k]);
+                if (min > dist) {
+                    min = dist;
+                }
+            }
+        }
+
+        return min;
+    }
+
+    @Override
+    protected double scoreFDSD(int matchingNGrams, int docLength) {
+        throw new UnsupportedOperationException();
+    }
 
 
-	@Override
-	public String getName() {
-		return "MDPF_DSM";
-	}
-	
+    @Override
+    public String getName() {
+        return "Min_Dist_Proximity_Feature_DSM";
+    }
+
 }

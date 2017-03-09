30b6f1fff0c28cf823cafb64351ebee9a1918fd8
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-09 01:28:25 +0000

    further tweaks

--- IR/ax2/DiffAvgPosProxFeatureDSM.java
+++ IR/ax2/DiffAvgPosProxFeatureDSM.java
@@ -10,15 +10,15 @@ import org.terrier.matching.dsms.DependenceScoreModifier;
 public class DiffAvgPosProxFeatureDSM extends DependenceScoreModifier {
 
 	@Override
-	protected double calculateDependence(IterablePosting[] postings, boolean[] okToUse, double[] phraseTermWeights, boolean SD) {
-		int numberOfQueryTerms = okToUse.length;
+	protected double calculateDependence(IterablePosting[] postings, boolean[] checks, double[] phraseTermWeights, boolean SD) {
+		int numberOfQueryTerms = checks.length;
 		int numberOfPairs = 0;
 		double score = 0.0d;
-		int len = postings.length
+		int len = postings.length;
 		List<Double> avgPosList = new ArrayList<>();
 
 		for (int i = 0; i < len; i++) {
-			if (okToUse[i]) {
+			if (checks[i]) {
 				BlockPosting blockPosting = (BlockPosting) postings[i]; 
 				int[] posList = blockPosting.getPositions();
 				double avg_pos = 0;

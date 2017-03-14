979c70ff5364b6eaff82ac45c8f2f345d768bc18
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-03-14 20:48:19 +0000

    added skeleton for bd ax2

---
+++ BD/ax2/src/main/java/uk/ac/glasgow/bd/ax1/format/MyInputFormat.java
@@ -0,0 +1,17 @@
+package main.java.uk.ac.glasgow.bd.ax1.format;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.LineRecordReader;
+
+
+public class MyInputFormat extends FileInputFormat<LongWritable, Text> {
+    public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) {
+        RecordReader recordReader = new LineRecordReader("\n\n".getBytes());
+        return recordReader;
+    }
+}
